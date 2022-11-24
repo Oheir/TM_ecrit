45bf9109470a531b9d703f8e4712aa357cecb2a7
Oheir <lgfanton@me.com>
2022-11-24 15:05:43 +0000

    addition des infos

--- source/tmconfig.py
+++ source/tmconfig.py
@@ -1,15 +1,15 @@
 class TMConfig:
 
-    title = u'Titre du TM'
-    first_name = 'hjsidaisjnd'
-    last_name = 'Nom'
+    title = u'Développement d’un site interactif pour l’apprentissage de l’arithmétique modulaire'
+    first_name = 'Louis-Guillaume'
+    last_name = 'Fanton'
     author = f'{first_name} {last_name}'
     year = u'2022'
     month = u'Décembre'
     seminary_title = u'Développement Web'
     tutor = u"Cédric Donner et Johan Jobin"
     release = "Version intermédiaire"
-    repository_url = "https://github.com/{your-docs-url}"
+    repository_url = "https://github.com/Oheir"
 
     @classmethod
     def date(cls):
