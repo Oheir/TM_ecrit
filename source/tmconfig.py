54e95a192e86dfdde979af3bfa08852a5828cef0
Oheir <lgfanton@me.com>
2023-03-30 12:44:22 +0000

    "fin reunion"

--- source/tmconfig.py
+++ source/tmconfig.py
@@ -3,13 +3,16 @@ class TMConfig:
     title = u'Développement d’un site interactif pour l’apprentissage de l’arithmétique modulaire'
     first_name = 'Louis-Guillaume'
     last_name = 'Fanton'
+    address = u"Chemin de la Reraise 46, 1616 Attalens"
     author = f'{first_name} {last_name}'
+    the_date = f"Le 30 mars 2023"
     year = u'2023'
     month = u'Avril'
     seminary_title = u'Développement Web'
     tutor = u"Cédric Donner et Johan Jobin"
-    release = "Version intermédiaire"
+    release = ""
     repository_url = "https://github.com/Oheir"
+    
 
     @classmethod
     def date(cls):
